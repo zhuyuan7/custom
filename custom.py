2c09206279da1de4f043f24f9a61a10187a4e7f7
JooJoo <68879766+zhuyuan7@users.noreply.github.com>
2021-10-08 18:35:41 +0900

    Update custom.py

--- custom.py
+++ custom.py
@@ -38,11 +38,8 @@ class CustomImageDataset(Dataset):
             image = self.transforms(image)
           
         return (image, y_label)
-<<<<<<< HEAD
-        
+
 
         
         
-=======
 
->>>>>>> 17e28690fd65096ed6396d322f202fd9512b243d
